3ff1c9d5a52915e418b1f6afe6c59b43cd3a2838
Midstackdev <midstackdev@gmail.com>
2021-11-17 20:12:21 +0000

    update transaction

--- backend/app/wallet/index.js
+++ backend/app/wallet/index.js
@@ -1,5 +1,6 @@
 import { STARTING_BALANCE } from "../../config.js";
 import { ec, cryptoHash } from "../../utils/index.js";
+import Transaction from "./transaction.js";
 
 class Wallet {
     constructor() {
@@ -12,6 +13,14 @@ class Wallet {
 
     sign(data) {
         return this.keyPair.sign(cryptoHash(data));
+    }
+
+    createTransaction({ recipient, amount }) {
+        if(amount > this.balance) {
+            throw new Error('Amount exceeds balance');
+        }
+
+        return new Transaction({ senderWallet: this, recipient, amount });
     }
 
 }
